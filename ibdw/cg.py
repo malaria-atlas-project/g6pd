e39d73029de8f1281da882bf98a9f13d6037ab14
Anand Patil <anand.prabhakar.patil@gmail.com>
2009-11-02 19:40:56 +0000

    Oops

--- ibdw/cg.py
+++ ibdw/cg.py
@@ -3,8 +3,8 @@ import imp
 imp.load_module('cut_geographic', *imp.find_module('cut_geographic',pm.gp.cov_funs.cov_utils.mod_search_path))
 from cut_geographic import cut_geographic as cg
 
-def cut_geographic(D,x,y,nx,ny,cmin=0,cmax=-1,symm=0):
+def cut_geographic(D,x,y,cmin=0,cmax=-1,symm=0):
     """
     A distance function that separates the Americas from the rest of the world.
     """
-    return cg(D,x,y,nx,ny,cmin,cmax,symm)
+    return cg(D,x,y,cmin,cmax,symm)

fba90bce74ceaf380682df32493c652925b53b5a
Anand Patil <anand.prabhakar.patil@gmail.com>
2011-06-21 08:57:11 -0700

    Edited g6pd/__init__.py via GitHub

--- g6pd/__init__.py
+++ g6pd/__init__.py
@@ -123,7 +123,7 @@ def area_fem_def(gc):
 areal_postproc = [area_male, area_fem_def_conservative, area_fem_def]
 
 def mcmc_init(M):
-    M.use_step_method(pm.gp.GPParentAdaptiveMetropolis, [M.amp, M.scale, M.diff_degree, M.ceiling, M.a, M.b])
+    M.use_step_method(pm.gp.GPParentAdaptiveMetropolis, [M.amp, M.scale, M.diff_degree, M.a, M.b])
     M.use_step_method(pm.gp.GPEvaluationGibbs, M.sp_sub, M.V, M.eps_p_f_d)
                     
 metadata_keys = ['fi','ti','ui']
